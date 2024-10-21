255e3214e88e5f66b08b263ced607031c3bad82c
anna-olivia <a.wietholtz@posteo.de>
2024-10-21 15:41:17 +0200

    21v37weiter595

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,46 @@
+import { useState } from 'react';
 import styles from './NewPost.module.css';
 
-const NewPost = ({onAuthorChange, onMessageChange, onCancel}) => {
+const NewPost = ({onCancel}) => {
+  const [currentMessage,setCurrentMessage] = useState('');
+  const [currentAuthor, setCurrentAuthor] = useState('');
+
+
+  const changeMessageHandler = (e) => {
+    setCurrentMessage(e.target.value);
+}
+
+const changeAuthorHandler = (e) => {
+    setCurrentAuthor(e.target.value);
+}
+const submitHandler = (e) => {
+  e.preventDefault();
+  // damit kein http request gesendet wird also dass es nicht zu einer kompleten neuladen der seite kommt sondern nur einer komponente aktualisiert wird und kein server side code benötigt wird- weil frontend also im browser läuft und damit react nicht umgehen könnte
+  const postData = {
+    message: currentMessage,
+    author: currentAuthor,
+  }
+  // object sammelt und gruppiert dann werte aus state also currentMessage und currentAuthor
+  // object fängt dann daten aus form ab ohne validierung (eigentlich müsste man noch client side validieren, ob richtiges eingegeben)
+  // hier ohne validierung aber wenigstens mit require attribut
+  console.log(postData);
+  onCancel();
+  // hier wird dann die onCancel function aufgerufen, damit beim klicken des senden buttons auch das modal wieder schließt
+  // hier halt unabhängig aufgerufen also sonst wär 
+
+}
+
   return (
-    <form className={styles.form}>
-      {/* p verschachteln aus semantischen Gründen - weil gruppierungen dann abgeschlossene informationseinheit ähnlich wie bei einem textabsatz - p darf aber nur inline elemente enthalten */}
+    <form className={styles.form} onSubmit={submitHandler}>
+      {/* eventListener onSubmit default event - hier sollen die Werte gesammelt werden die vorher in PostLists unter den Props von post lagen - jetzt sollen da aber halt aus ner Schleife dann Post entstehen - also kann auch State dort rausgenommen werden*/}
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onMessageChange}/>
+        <textarea id="body" required rows={3} onChange={changeMessageHandler}/>
       </p>
+      {/* p verschachteln aus semantischen Gründen - weil gruppierungen dann abgeschlossene informationseinheit ähnlich wie bei einem textabsatz - p darf aber nur inline elemente enthalten */}
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" onChange={onAuthorChange} required />
+        <input type="text" id="name" onChange={changeAuthorHandler} required />
       </p>
       {/* hier sollte man besser div oder section statt p zur verschachtelung verwenden weil button blockelemente */}
       <section className={styles.actions}>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from "./Post";
@@ -7,31 +7,18 @@ import styles from './PostsList.module.css';
 // state uplifting, damit alles in app.jsx verfügbar gemacht wird
 
 const PostsList = ({isPosting, onStopPosting}) => {
-    const [currentMessage,setCurrentMessage] = useState('');
-    const [currentAuthor, setCurrentAuthor] = useState('');
-  
-
-    const changeMessageHandler = (e) => {
-        setCurrentMessage(e.target.value);
-    }
-    
-    const changeAuthorHandler = (e) => {
-        setCurrentAuthor(e.target.value);
-    }
-
-
+   
     return(
         <>
 {/* value name geändert damit es leichter weitergegeben werden kann, übersichtlciher wird und nicht verwechselt wird mit handlerfkt - hier soll onStopPosting auch nur ein Pointer auf closeModalHandler werden - isPosting is dann modalVisible Wert*/}
         {isPosting ?  <Modal onClose={onStopPosting}>
-        <NewPost
-        onMessageChange={changeMessageHandler} onAuthorChange={changeAuthorHandler} onCancel={onStopPosting}/>
+        <NewPost onCancel={onStopPosting}/>
         </Modal> : false }
          {/* kann auch null schreiben beides in dem fall das gleiche weil modal ja nicht gezeigt werden soll also wird useState auf false gesetzt oder null was dann das gleiche bedeuten würde */}
 
        
         <ul className={styles.posts}>
-             <Post author={currentAuthor}  message={currentMessage}/>
+            {/* Post und zugehöriger state rausgenommen weil dynamisch mit eintrag bzw. form submission erstellt werden soll */}
              <Post author="anna" message="hallo"/>
 
         </ul>
